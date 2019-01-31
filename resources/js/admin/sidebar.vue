4603c17d5977b9dfcb2d721af62d0be212d12806
dexstarious <31502399+dexsterious@users.noreply.github.com>
2019-01-31 16:34:52 +0800

    new

--- resources/js/admin/sidebar.vue
+++ resources/js/admin/sidebar.vue
@@ -1,6 +1,9 @@
 <template>
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <router-link to="#" class="brand-link text-center">
+        <router-link to="#" class="brand-link">
+        <!-- Brand Logo -->
+            <img :src="'/img/bghmc.png'" alt="BGHMC Logo" class="brand-image img-circle elevation-3"
+                 style="opacity: .8">
           <span class="brand-text font-weight-light">PMIS</span>
         </router-link>
 
